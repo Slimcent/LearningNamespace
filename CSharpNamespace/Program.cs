1e3121a5a7d399e192369c003ea20740a825c64d
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-11-19 22:24:01 +0100

    namespace project.

---
+++ CSharpNamespace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WelcomeMessageNameSpace;
+
+namespace CSharpNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage message = new WelcomeMessage();
+            message.GreetMessage();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace WelcomeMessageNameSpace
+{
+    class WelcomeMessage
+    {
+        public void GreetMessage()
+        {
+            Console.WriteLine("Welcome to C Sharp namespace");
+        }
+    }
+}
